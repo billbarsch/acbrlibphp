fbb9bca65299e60ff328308720eadbed877340cb
Bill Barsch <billbarsch@gmail.com>
2020-05-27 23:17:53 -0300

    add schemas

---
+++ AtualizaSchemas.bat
@@ -0,0 +1,6 @@
+rd /s /q "C:\Users\billb\OneDrive\ProjetosLazarus\acbrlibphp\Schemas"
+xcopy  D:\lazarus\components\acbr\Exemplos\ACBrDFe\Schemas C:\Users\billb\OneDrive\ProjetosLazarus\acbrlibphp\Schemas /e /i /h
+cd Schemas
+md Nfse
+xcopy  D:\lazarus\components\acbr\Exemplos\ACBrDFe\ACBrNFSe\Schemas\* C:\Users\billb\OneDrive\ProjetosLazarus\acbrlibphp\Schemas\Nfse /e /i /h
+xcopy  D:\lazarus\components\acbr\Exemplos\ACBrDFe\ACBrNFSe\ArqINI C:\Users\billb\OneDrive\ProjetosLazarus\acbrlibphp\Schemas\ArqINI /e /i /h
